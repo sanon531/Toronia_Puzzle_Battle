eaf497184d12d63d0ae45ad3245a37c8ee66dd20
ohsangwon <axur123@gmail.com>
2021-09-22 14:18:05 +0900

    컨베이어용 무한 스크롤 추가 및 임시 스프라이트들 추가

---
+++ Assets/2_ExternalTool/InfiniteScroll/Loop/ItemControllerLoop.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(InfiniteScroll))]
+public class ItemControllerLoop : UIBehaviour, IInfiniteScrollSetup
+{
+    [SerializeField]
+    ScrollRect m_scrollRect;
+ 
+
+
+    protected override void Start()
+    {
+       
+    }
+
+    private bool isSetuped = false;
+
+	public void OnPostSetupItems()
+	{
+		GetComponentInParent<ScrollRect>().movementType = ScrollRect.MovementType.Unrestricted;
+		isSetuped = true;
+	}
+
+	public void OnUpdateItem(int itemCount, GameObject obj)
+	{
+		if(isSetuped == true) return;
+
+		//var item = obj.GetComponentInChildren<BlockConveyCase>();
+		//item.UpdateItem(itemCount);
+	}
+
+    
+}
